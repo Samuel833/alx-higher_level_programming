4cd83ad09558d32f6bc577f8f6bec6338cd3ac06
Sowunmi Oluwasegun Samuel <samuelsegun833@gmail.com>
2022-09-06 02:07:05 +0100

    Made files executable

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing'; // string
-
-console.log(myVar); // console.log(myVar) will print 'JavaScript is amazing'
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const Astr = 'C is fun';
-const Bstr = 'Python is cool';
-const Cstr = 'Javascript is amazing';
-
-console.log(Astr); // console.log(Astr) will print 'C is fun'
-console.log(Bstr); // console.log(Bstr) will print 'Python is cool'
-console.log(Cstr); // console.log(Cstr) will print 'Javascript is amazing'
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
